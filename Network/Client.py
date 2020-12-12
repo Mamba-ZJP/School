bfd0e8d54143d3a8f853eba388d4e608b6a90809
Mamba-ZJP <1330472951@qq.com>
2020-12-12 21:48:03 +0800

    Add encryption part.

--- Network/Client.py
+++ Network/Client.py
@@ -1,18 +1,32 @@
 from socket import * 
+import base64
 
-serverAddr = "192.168.43.230"
+serverAddr = input("Server address:") # server ip  192.168.8.108
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket = socket(AF_INET, SOCK_STREAM) 
 clientSocket.connect((serverAddr, serverPort))
+
+def encrypt(plaintext):   # 加密明文
+    encryptedText = base64.b64encode(plaintext.encode("utf-8"))  # 先将字符串转为 2 进制 (utf-8：一种二进制编码格式)
+    return encryptedText
+
+def decrypt(ciphertext):
+    decryptedText = base64.b64decode(ciphertext).decode("utf-8")
+    return decryptedText
 
 while 1:
     sentence = input("Client input : ")
-    clientSocket.send(sentence.encode()) # why encode()
+    clientSocket.send( encrypt(sentence) )  # why encode()
 
-    response = clientSocket.recv(1024).decode()
+    response = decrypt( clientSocket.recv(1024) )
     print ('Server answer: ', response)
-    # clientSocket.close()
+
 
 clientSocket.close()
+
+
+
+
+
 
 
--- Network/Server.py
+++ Network/Server.py
@@ -1,19 +1,32 @@
 from socket import *
+import base64
+
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM) # create socket
 serverSocket.bind(('', serverPort)) # bind socket and port
-serverSocket.listen(1) # 请求连接的最大数
+serverSocket.listen(1)  # 请求连接的最大数
+
 print ("The server is ready!")
 connectSocket, addr = serverSocket.accept() # 接收连接请求
 
+def encrypt(plaintext):   # 加密明文
+    encryptedText = base64.b64encode(plaintext.encode("utf-8"))  # 先将字符串转为 2 进制
+    return encryptedText
+
+def decrypt(ciphertext):
+    decryptedText = base64.b64decode(ciphertext).decode("utf-8")
+    return decryptedText
+
+
 while 1:
-    sentence = connectSocket.recv(1024).decode() # bufsize
+    sentence = decrypt( connectSocket.recv(1024) )  # bufsize
     print("Client says: ", sentence)
     response = input("Your answer: ")
-    connectSocket.send(response.encode())
+    connectSocket.send( encrypt(response) )
 
 connectSocket.close()
+
 
 
 
