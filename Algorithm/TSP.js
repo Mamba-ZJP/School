b8f2be0b43d7ab2ee601ff53600eed7456d970a5
Mamba-ZJP <1330472951@qq.com>
2020-12-27 22:42:44 +0800

    Animiation of TSP using ACO

--- Algorithm/TSP.js
+++ Algorithm/TSP.js
@@ -1,14 +1,22 @@
+const canvas = document.querySelector(".myCanvas");
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
+ctx.strokeStyle = "rgb(0, 0, 0)";
+ctx.lineWidth = 1;
+
 let alpha, beta, rho, Q, antNumber, maxIterations, antGroup, pheromone, cityNumber;
 // 信息素因子, 启发函数因子, 信息挥发因子, 信息素常量, 蚂蚁数量, 最大迭代次数, 蚂蚁群, 信息素矩阵
 const mapX = [178,272,176,171,650,499,267,703,408,437,491,74,532,
     416,626,42,271,359,163,508,229,576,147,560,35,714,
     757,517,64,314,675,690,391,628,87,240,705,699,258,
-    428,614,36,360,482,666,597,209,201,492,294];
+    428];
 
 const mapY = [170,395,198,151,242,556,57,401,305,421,267,105,525,
     381,244,330,395,169,141,380,153,442,528,329,232,48,
     498,265,343,120,165,50,433,63,491,275,348,222,288,
-    490,213,524,244,114,104,552,70,425,227,331];
+    490];
 
 function Ant(startCity) {
     this.path = [startCity];
@@ -86,11 +94,17 @@ function Ant(startCity) {
         this.path.push(nextCity);
         this.hasVisitedCity.push(nextCity);
         this.sumOfDistance += Distance(currentCity, nextCity);
+
+        ctx.beginPath();
+
+        ctx.moveTo(mapX[currentCity], mapY[currentCity]);
+        ctx.lineTo(mapX[nextCity], mapY[nextCity]);
+        ctx.closePath();
+        ctx.stroke();
     }
 };
 
 function Distance(currentCity, nextCity) {
-    // console.log(currentCity + ', ' + nextCity);
     let dist = Math.pow(mapX[currentCity] - mapX[nextCity], 2) + 
                Math.pow(mapY[currentCity] - mapY[nextCity], 2);
     return Math.sqrt(dist);
@@ -98,7 +112,7 @@ function Distance(currentCity, nextCity) {
 
 function Initial() {
     alpha = 3.0, beta = 3.0, rho = 0.65, Q = 100.0;
-    antNumber = 50, maxIterations = 150, antGroup = [];
+    antNumber = 20, maxIterations = 150, antGroup = [];
     cityNumber = mapX.length;
 
     // console.log(cityNumber);
@@ -110,20 +124,6 @@ function Initial() {
             pheromone[i][j] = 1.0;
         }
     }
-    
-    
-    // let sumLength = 0.0;
-    // for (let i = 0; i < map.length - 1; i++) {
-    //     sumLength += Distance(i, i + 1);
-    // }
-    // sumLength += Distance(map.length - 1, 0);
-
-    // for (let i = 0; i < map.length - 1; i++) {
-    //     pheromone[i][i + 1] += (antNumber / sumLength);
-    //     pheromone[i + 1][i] += (antNumber / sumLength);
-    // }
-    // pheromone[map.length - 1][0] += (antNumber / sumLength);
-    // pheromone[0][map.length - 1] += (antNumber / sumLength);
 };
 
 function UpdatePheromone() {
@@ -153,29 +153,48 @@ function UpdatePheromone() {
 }
 
 
-function SetAnt() {
+function Reset() {
     antGroup = [];
     for (let i = 0; i < antNumber; i++) {
         let randomCity = Math.floor(Math.random() * cityNumber);
         antGroup.push(new Ant(randomCity));
     }
+
+}
+
+function ClearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+} 
+
+function loop() {
+    let minimalDistance = Math.pow(2, 30);
+    Reset();
+    for (let j = 0; j < antNumber; j++) {
+        ClearCanvas();
+        antGroup[j].Travel();
+        // antGroup[j].Travel();
+        minimalDistance = Math.min(minimalDistance, antGroup[j].sumOfDistance);
+    }
+    UpdatePheromone();
+    // console.log("第" + iterations + "次迭代, 最短路径长度为: " + minimalDistance);    
+    // if (iterations++ < maxIterations)
+        requestAnimationFrame(loop);
 }
 
 function TSP() {
     let minimalDistance = Math.pow(2, 30);
     Initial();
-    for (let i = 1; i <= maxIterations; i++) {
-        SetAnt();
-        for (let j = 0; j < antNumber; j++) {
-            antGroup[j].Travel();
-            // console.log(antGroup[j].sumOfDistance);
-            minimalDistance = Math.min(minimalDistance, antGroup[j].sumOfDistance);
-        }
-        // console.log("next");
-        UpdatePheromone();
-        console.log("第" + i + "次迭代, 最短路径长度为: " + minimalDistance);
-    }
-    console.log(minimalDistance);
+    // let iterations = 0;
+    loop();
+
+    // for (let i = 1; i <= maxIterations; i++) {
+    //     Reset();
+    //     window.requestAnimationFrame(run());
+    //     // console.log("next");
+    //     UpdatePheromone();
+    //     console.log("第" + i + "次迭代, 最短路径长度为: " + minimalDistance);
+    // }
+    // console.log(minimalDistance);
 }
 
 TSP();
