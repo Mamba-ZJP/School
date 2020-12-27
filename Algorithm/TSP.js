cddae0e76930c0459dac89b6fc556bbbb2cd29bd
Mamba-ZJP <1330472951@qq.com>
2020-12-27 12:16:29 +0800

    Finish programs of TSP using Ant Colony Optimization, but there are still some problems.

--- Algorithm/TSP.js
+++ Algorithm/TSP.js
@@ -1,35 +1,35 @@
 let alpha, beta, rho, Q, antNumber, maxIterations, antGroup, pheromone, cityNumber;
-// 信息素因子, 启发函数因子, 信息挥发因子, 信息素常量, 蚂蚁数量, 最大迭代次数, 蚂蚁群
-const map = [[11.0, 12.0], [144.1, 2.1], [70.8, 121.9]];
+// 信息素因子, 启发函数因子, 信息挥发因子, 信息素常量, 蚂蚁数量, 最大迭代次数, 蚂蚁群, 信息素矩阵
+const map = [];
 
-class Ant {
-    constructor(startCity) {
-        this.path.push(startCity);
-        this.hasVisitedCity.push(startCity);
-    }
+function Ant(startCity) {
+    this.path = [startCity];
+    this.hasVisitedCity = [startCity];
+    this.sumOfDistance = 0.0;
 
-    path = [];
-    hasVisitedCity = [];
-    sumOfDistance = 0.0;
-
-    Travel() {
+    this.Travel = function() {
         for (let i = 0; i < cityNumber - 1; i++) {
             let currentCity = this.path[this.path.length - 1];
             let sum = this.CalculateDown(currentCity);
-            let nextCity = this.ChooseNextCity(sum);
-            Update(nextCity);    
+            let nextCity = this.ChooseNextCity(currentCity, sum);
+            this.Update(currentCity, nextCity);
         }
+        this.GoBackStart();    
+    };
+
+    this.GoBackStart = function() {
+        this.sumOfDistance += Distance(this.path[this.path.length - 1], this.path[0]);
+        this.path.push(this.path[0]);
     }
 
-    ChooseNextCity(sum) {
-        // 从剩下的城市里挑一个最可能去的
-        let city = currentCity, maximalPossibility = 0.0;
+    this.ChooseNextCity = function(currentCity, sum) {
+        let city, maximalPossibility = 0.0;
         for (let nextCity = 0; nextCity < cityNumber; nextCity++) {
-            if (this.hasVisitedCity.find(nextCity) === undefined) {
+            if (this.hasVisitedCity.indexOf(nextCity) !== -1) {
                 continue;
             }
             let possibility = (Math.pow(pheromone[currentCity][nextCity], alpha) * 
-                               Math.pow(Distance(currentCity, nextCity), beta) / sum);
+                               Math.pow(1 / Distance(currentCity, nextCity), beta) / sum);
 
             if (possibility > maximalPossibility) {
                 maximalPossibility = possibility, city = nextCity;
@@ -37,51 +37,116 @@ class Ant {
         }
 
         return city;
-    }
-
-    CalculateDown(currentCity) {
-        let sum = 0.0, 
+    };
+    
+    this.CalculateDown = function(currentCity) {
+        let sum = 0.0;
         for (let nextCity = 0; nextCity < cityNumber; nextCity++) {
-            if (this.hasVisitedCity.find(nextCity) === undefined) {
+            if (this.hasVisitedCity.indexOf(nextCity) !== -1) {
                 continue;
             }
+            
             sum += (Math.pow(pheromone[currentCity][nextCity], alpha) * 
-                    Math.pow(Distance(currentCity, nextCity), beta));
+                    Math.pow(1 / Distance(currentCity, nextCity), beta));
         }
+
         return sum;
-    }
+    };
 
-    Distance(currentCity, nextCity) {
-        let dist = Math.pow(map[currentCity][0] - map[nextCity][0]) + 
-                   Math.pow(map[currentCity][1] - map[nextCity][1]);
-        return Math.floor(Math.sqrt(dist));
-    }
-
-    Update(nextCity) {
+    this.Update = function(currentCity, nextCity) {
         this.path.push(nextCity);
         this.hasVisitedCity.push(nextCity);
+        this.sumOfDistance += Distance(currentCity, nextCity);
     }
+};
+
+function Distance(currentCity, nextCity) {
+    // console.log(currentCity + ', ' + nextCity);
+    let dist = Math.pow(map[currentCity][0] - map[nextCity][0], 2) + 
+               Math.pow(map[currentCity][1] - map[nextCity][1], 2);
+    return Math.sqrt(dist);
 }
 
 function Initial() {
-    alpha = 2.0, beta = 1.0, rho = 0.3, Q = 40.0;
-    antNumber = 5, maxIterations = 50, antGroup = [], cityNumber = map.length;
+    alpha = 2.0, beta = 2.0, rho = 0.45, Q = 40.0;
+    antNumber = 30, maxIterations = 50, antGroup = [];
+    cityNumber = 30;
+    for (let i = 0; i < cityNumber; i++) {
+        let city = [Math.random() * 1000, Math.random() * 1000];
+        map.push(city);
+    }
+    // console.log(cityNumber);
+    // 二维信息素矩阵
+    pheromone = new Array(map.length);
+    for (let i = 0; i < cityNumber; i++) {
+        pheromone[i] = new Array(map.length);
+        for (let j = 0; j < map.length; j++)
+            pheromone[i][j] = 0.0;
+    }
+    
+    
+    let sumLength = 0.0;
+    for (let i = 0; i < map.length - 1; i++) {
+        sumLength += Distance(i, i + 1);
+    }
+    sumLength += Distance(map.length - 1, 0);
+
+    for (let i = 0; i < map.length - 1; i++) {
+        pheromone[i][i + 1] += (antNumber / sumLength);
+        pheromone[i + 1][i] += (antNumber / sumLength);
+    }
+    pheromone[map.length - 1][0] += (antNumber / sumLength);
+    pheromone[0][map.length - 1] += (antNumber / sumLength);
+};
+
+function UpdatePheromone() {
+    let deltaPheromone = new Array(cityNumber);
+    for (let i = 0; i < cityNumber; i++) {
+        deltaPheromone[i] = new Array(cityNumber);
+        for (let j = 0; j < cityNumber; j++) {
+            deltaPheromone[i][j] = 0.0;
+        }
+    }
+
+    for (let i = 0; i < antGroup.length; i++) {
+        let ant = antGroup[i];
+        for (let currentCity = ant.path[0]; currentCity < ant.length - 1; currentCity++) {
+            let nextCity = ant.path[currentCity + 1];
+            deltaPheromone[currentCity][nextCity] += (Q / ant.sumOfDistance);
+            deltaPheromone[nextCity][currentCity] += (Q / ant.sumOfDistance);
+        }
+    }
+
+    for (let i = 0; i < cityNumber; i++) {
+        for (let j = 0; j < cityNumber; j++) {
+            pheromone[i][j] *= (1 - rho);
+            pheromone[i][j] += deltaPheromone[i][j];
+        }
+    }
+}
+
+
+function SetAnt() {
+    antGroup = [];
     for (let i = 0; i < antNumber; i++) {
         let randomCity = Math.floor(Math.random() * cityNumber);
         antGroup.push(new Ant(randomCity));
     }
-};
-
-function UpdatePheromone() {
-    
 }
 
 function TSP() {
+    let minimalDistance = Math.pow(2, 30);
     Initial();
     for (let i = 0; i < maxIterations; i++) {
+        SetAnt();
         for (let j = 0; j < antNumber; j++) {
             antGroup[j].Travel();
+            minimalDistance = Math.min(minimalDistance, antGroup[j].sumOfDistance);
         }
         UpdatePheromone();
+        console.log(minimalDistance);
     }
+    console.log(minimalDistance);
 }
+
+TSP();
