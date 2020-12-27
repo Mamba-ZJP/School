d71853b6601c59d09f963874e7e6c5a463f1d053
Mamba-ZJP <1330472951@qq.com>
2020-12-27 16:00:54 +0800

    Finish TSP using ACO, which used lunPanDu.

--- Algorithm/TSP.js
+++ Algorithm/TSP.js
@@ -1,6 +1,14 @@
 let alpha, beta, rho, Q, antNumber, maxIterations, antGroup, pheromone, cityNumber;
 // 信息素因子, 启发函数因子, 信息挥发因子, 信息素常量, 蚂蚁数量, 最大迭代次数, 蚂蚁群, 信息素矩阵
-const map = [];
+const mapX = [178,272,176,171,650,499,267,703,408,437,491,74,532,
+    416,626,42,271,359,163,508,229,576,147,560,35,714,
+    757,517,64,314,675,690,391,628,87,240,705,699,258,
+    428,614,36,360,482,666,597,209,201,492,294];
+
+const mapY = [170,395,198,151,242,556,57,401,305,421,267,105,525,
+    381,244,330,395,169,141,380,153,442,528,329,232,48,
+    498,265,343,120,165,50,433,63,491,275,348,222,288,
+    490,213,524,244,114,104,552,70,425,227,331];
 
 function Ant(startCity) {
     this.path = [startCity];
@@ -13,8 +21,11 @@ function Ant(startCity) {
             let sum = this.CalculateDown(currentCity);
             let nextCity = this.ChooseNextCity(currentCity, sum);
             this.Update(currentCity, nextCity);
+            // console.log("目前在" + currentCity + ", 距离为" + this.sumOfDistance);
         }
-        this.GoBackStart();    
+        this.GoBackStart();   
+        // console.log(this.sumOfDistance);
+        // console.log("next"); 
     };
 
     this.GoBackStart = function() {
@@ -23,19 +34,37 @@ function Ant(startCity) {
     }
 
     this.ChooseNextCity = function(currentCity, sum) {
-        let city, maximalPossibility = 0.0;
+        // 轮盘赌法
+        let city, maximalPossibility = 0.0, sumPossibility = 0.0;
+        let allowedCity = [];
         for (let nextCity = 0; nextCity < cityNumber; nextCity++) {
             if (this.hasVisitedCity.indexOf(nextCity) !== -1) {
                 continue;
             }
             let possibility = (Math.pow(pheromone[currentCity][nextCity], alpha) * 
                                Math.pow(1 / Distance(currentCity, nextCity), beta) / sum);
-
-            if (possibility > maximalPossibility) {
-                maximalPossibility = possibility, city = nextCity;
-            }
+            sumPossibility += possibility;
+            allowedCity.push([nextCity, possibility]);
         }
 
+        allowedCity.forEach((el) => {
+            el[1] = el[1] / sumPossibility;
+        });
+        
+        allowedCity[0].push(allowedCity[0][1]);
+        for (let i = 1; i < allowedCity.length; i++) {
+            let accumlatedPossibility = allowedCity[i - 1][2] + allowedCity[i][1];
+            allowedCity[i].push(accumlatedPossibility);
+        }
+        
+        let randomNumber = Math.random();
+        for (let i = 1; i < allowedCity.length; i++) {
+            if (randomNumber <= allowedCity[i][2]) {
+                city = i;
+                break;
+            }
+        }
+        // console.log("city: " + city);
         return city;
     };
     
@@ -62,41 +91,39 @@ function Ant(startCity) {
 
 function Distance(currentCity, nextCity) {
     // console.log(currentCity + ', ' + nextCity);
-    let dist = Math.pow(map[currentCity][0] - map[nextCity][0], 2) + 
-               Math.pow(map[currentCity][1] - map[nextCity][1], 2);
+    let dist = Math.pow(mapX[currentCity] - mapX[nextCity], 2) + 
+               Math.pow(mapY[currentCity] - mapY[nextCity], 2);
     return Math.sqrt(dist);
 }
 
 function Initial() {
-    alpha = 2.0, beta = 2.0, rho = 0.45, Q = 40.0;
-    antNumber = 30, maxIterations = 50, antGroup = [];
-    cityNumber = 30;
-    for (let i = 0; i < cityNumber; i++) {
-        let city = [Math.random() * 1000, Math.random() * 1000];
-        map.push(city);
-    }
+    alpha = 3.0, beta = 3.0, rho = 0.65, Q = 100.0;
+    antNumber = 50, maxIterations = 150, antGroup = [];
+    cityNumber = mapX.length;
+
     // console.log(cityNumber);
     // 二维信息素矩阵
-    pheromone = new Array(map.length);
+    pheromone = new Array(cityNumber);
     for (let i = 0; i < cityNumber; i++) {
-        pheromone[i] = new Array(map.length);
-        for (let j = 0; j < map.length; j++)
-            pheromone[i][j] = 0.0;
+        pheromone[i] = new Array(cityNumber);
+        for (let j = 0; j < cityNumber; j++) {
+            pheromone[i][j] = 1.0;
+        }
     }
     
     
-    let sumLength = 0.0;
-    for (let i = 0; i < map.length - 1; i++) {
-        sumLength += Distance(i, i + 1);
-    }
-    sumLength += Distance(map.length - 1, 0);
+    // let sumLength = 0.0;
+    // for (let i = 0; i < map.length - 1; i++) {
+    //     sumLength += Distance(i, i + 1);
+    // }
+    // sumLength += Distance(map.length - 1, 0);
 
-    for (let i = 0; i < map.length - 1; i++) {
-        pheromone[i][i + 1] += (antNumber / sumLength);
-        pheromone[i + 1][i] += (antNumber / sumLength);
-    }
-    pheromone[map.length - 1][0] += (antNumber / sumLength);
-    pheromone[0][map.length - 1] += (antNumber / sumLength);
+    // for (let i = 0; i < map.length - 1; i++) {
+    //     pheromone[i][i + 1] += (antNumber / sumLength);
+    //     pheromone[i + 1][i] += (antNumber / sumLength);
+    // }
+    // pheromone[map.length - 1][0] += (antNumber / sumLength);
+    // pheromone[0][map.length - 1] += (antNumber / sumLength);
 };
 
 function UpdatePheromone() {
@@ -137,14 +164,16 @@ function SetAnt() {
 function TSP() {
     let minimalDistance = Math.pow(2, 30);
     Initial();
-    for (let i = 0; i < maxIterations; i++) {
+    for (let i = 1; i <= maxIterations; i++) {
         SetAnt();
         for (let j = 0; j < antNumber; j++) {
             antGroup[j].Travel();
+            // console.log(antGroup[j].sumOfDistance);
             minimalDistance = Math.min(minimalDistance, antGroup[j].sumOfDistance);
         }
+        // console.log("next");
         UpdatePheromone();
-        console.log(minimalDistance);
+        console.log("第" + i + "次迭代, 最短路径长度为: " + minimalDistance);
     }
     console.log(minimalDistance);
 }
